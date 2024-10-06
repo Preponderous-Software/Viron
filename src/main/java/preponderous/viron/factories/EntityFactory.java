4f79ab8ca1c68cdd3c34612f1620d13fb3131df4
dmccoystephenson <dmccoystephenson@gmail.com>
2024-10-05 18:03:37 -0600

    Created some factory skeletons with TODOs

---
+++ src/main/java/preponderous/viron/factories/EntityFactory.java
@@ -0,0 +1,10 @@
+package preponderous.viron.factories;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EntityFactory {
+    // TODO: implement method to create entity
+
+    // TODO: implement method to delete entity
+}
---
+++ src/main/java/preponderous/viron/factories/EnvironmentFactory.java
@@ -0,0 +1,10 @@
+package preponderous.viron.factories;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvironmentFactory {
+    // TODO: implement method to create environment (and underlying grids/locations)
+
+    // TODO: implement method to delete environment (and underlying grids/locations)
+}

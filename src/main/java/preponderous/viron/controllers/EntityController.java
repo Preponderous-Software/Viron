04bd8c970d0451d9bcbac347b17fa6b3bb8460bf
dmccoystephenson <dmccoystephenson@gmail.com>
2024-10-09 22:30:47 -0600

    Implemented more entity endpoints & added logger to entity controller

--- src/main/java/preponderous/viron/controllers/EntityController.java
+++ src/main/java/preponderous/viron/controllers/EntityController.java
@@ -8,22 +8,29 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import preponderous.viron.database.DbInteractions;
+import preponderous.viron.factories.EntityFactory;
 import preponderous.viron.models.Entity;
 
 @RestController
 @RequestMapping("/entity")
 public class EntityController {
     private final DbInteractions dbInteractions;
+    private final EntityFactory entityFactory;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public EntityController(DbInteractions dbInteractions) {
+    public EntityController(DbInteractions dbInteractions, EntityFactory entityFactory) {
         this.dbInteractions = dbInteractions;
+        this.entityFactory = entityFactory;
     }
 
     @RequestMapping("/get-all-entities")
@@ -38,7 +45,7 @@ public class EntityController {
                 entities.add(new Entity(id, name, creationDate));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Error getting entities: " + e.getMessage());
         }
         return entities;
     }
@@ -53,7 +60,7 @@ public class EntityController {
                 return new Entity(id, name, creationDate);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Error getting entity by id: " + e.getMessage());
         }
         return null;
     }
@@ -70,7 +77,7 @@ public class EntityController {
                 entities.add(new Entity(id, name, creationDate));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Error getting entities in environment: " + e.getMessage());
         }
         return entities;
     }
@@ -87,7 +94,7 @@ public class EntityController {
                 entities.add(new Entity(id, name, creationDate));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Error getting entities in grid: " + e.getMessage());
         }
         return entities;
     }
@@ -104,14 +111,47 @@ public class EntityController {
                 entities.add(new Entity(id, name, creationDate));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Error getting entities in location: " + e.getMessage());
         }
         return entities;
     }
 
-    // TODO: implement 'create' endpoint
+    @RequestMapping("/get-entities-not-in-any-location")
+    public List<Entity> getEntitiesNotInAnyLocation() {
+        List<Entity> entities = new ArrayList<>();
+        ResultSet rs = dbInteractions.query("SELECT * FROM viron.entity WHERE entity_id not in (SELECT entity_id FROM viron.entity_location)");
+        try {
+            while (rs.next()) {
+                int id = rs.getInt("entity_id");
+                String name = rs.getString("name");
+                String creationDate = rs.getString("creation_date");
+                entities.add(new Entity(id, name, creationDate));
+            }
+        } catch (SQLException e) {
+            logger.error("Error getting entities not in any location: " + e.getMessage());
+        }
+        return entities;
+    }
 
-    // TODO: implement 'delete' endpoint
+    @RequestMapping("/create-entity/{name}")
+    public Entity createEntity(@PathVariable String name) {
+        try {
+            return entityFactory.createEntity(name);
+        } catch (EntityFactory.EntityCreationException e) {
+            logger.error("Error creating entity: " + e.getMessage());
+            return null;
+        }
+    }
 
-    // TODO: implement 'set name' endpoint
+    @RequestMapping("/delete-entity/{id}")
+    public boolean deleteEntity(@PathVariable int id) {
+        String query = "DELETE FROM viron.entity WHERE entity_id = " + id;
+        return dbInteractions.update(query);
+    }
+
+    @RequestMapping("/set-entity-name/{id}/{name}")
+    public boolean setEntityName(@PathVariable int id, @PathVariable String name) {
+        String query = "UPDATE viron.entity SET name = '" + name + "' WHERE entity_id = " + id;
+        return dbInteractions.update(query);
+    }
 }
